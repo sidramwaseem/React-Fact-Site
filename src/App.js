00628e415283ecce2aabc721c0f5bc743e7edee6
sidramwaseem <sidraw24@gmail.com>
2022-05-15 22:03:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Navbar from "./components/Navbar.js";
 import Main from "./components/Main.js";
 
 export default function App() {
+  const [lightMode, setLightMode] = React.useState(true);
+  function toggleLightMode(){
+    setLightMode(prevMode => !prevMode)
+  }
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar lightMode={lightMode} toggleLightMode={toggleLightMode} />
+      <Main lightMode={lightMode} />
     </div>
   );
 }
