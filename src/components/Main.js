4eff004bab7ee0965438962ec2d1e4ddc9d87aaf
sidramwaseem <sidraw24@gmail.com>
2022-05-15 22:01:03 +0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React from "react";
 // import logo from "../images/reactLogo.png"
 
-export default function Main() {
+export default function Main(props) {
+
   return (
-    <main className="main--section">
+    <main className={props.lightMode? "light main--section main--section--light" : "main--section"}>
       <h1 className="facts--heading">Fun facts about React</h1>
       <ul className="facts--list">
         <li>Was first released in 2013</li>
