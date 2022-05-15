92defec4e5e30f3dfa3031c2572a852e8a5502a4
sidramwaseem <sidraw24@gmail.com>
2022-05-15 22:01:12 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactLogo from "../images/reactLogo.png";
-export default function Navbar() {
+// import Dark from "../images/light-toggle-dark.png";
+export default function Navbar(props) {
   return (
-    <div className="navbar">
+    <div className={props.lightMode ? "light navbar" : "navbar"}>
       <div className="nav-logo">
         <img className="reactLogo" src={ReactLogo} alt="reactLogo" />
         <h1>ReactFacts</h1>
       </div>
-      <p>React Course - Project 1</p>
+     <p> <span className={props.lightMode?"lightText":"dark"}>Light</span>  {<i onClick={props.toggleLightMode} class={props.lightMode ? "fa-solid fa-toggle-off fa-2x" : "fa-solid fa-toggle-on fa-2x"}></i>} <span className={props.lightMode?"lightText":"dark"}>Dark</span> </p>
     </div>
   );
 }
+ //<i class="fa-solid fa-toggle-off"></i>
